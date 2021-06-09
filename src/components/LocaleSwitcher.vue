93ea35d1429114258afcce60de4f6cc2138c30aa
Kleine Schlampe <github.com/SchlampeKleine>
2021-06-09 19:55:17 +0200

    add own locale switcher

---
+++ src/components/LocaleSwitcher.vue
@@ -0,0 +1,20 @@
+<template>
+<select v-model="locale">
+  <option value="en">en</option>
+  <option value="de">de</option>
+</select>
+</template>
+
+<script>
+import { useI18n } from 'vue-i18n';
+
+export default {
+  name: 'LocaleSwitcher',
+  setup() {
+    const { locale } = useI18n({ useScope: 'global' });
+    return {
+      locale,
+    };
+  },
+};
+</script>
