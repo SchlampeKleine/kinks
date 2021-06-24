a8a4c62ef8c56930b22e095c28cf43b828feb30e
Kleine Schlampe <github.com/SchlampeKleine>
2021-06-24 06:31:18 +0200

    update credits and privacy
    change formatting
    remove 'removal note' after getting the permission to use the kinklist

--- src/views/Credits.vue
+++ src/views/Credits.vue
@@ -1,13 +1,14 @@
 <template>
-  <h1 class="title">Sources</h1>
-  <p> The content of this list is at the moment mostly imported from
-  <a
-    href="http://discerningspecialist.com/articles/bdsm-101/34-bdsm-checklist" >
-    DiscerningSpecialist
-  </a>
-    but as I have no rights for that
-    I will probably need to start the list of kinks over.
-  </p>
+  <section class="section">
+    <h1 class="title">Sources</h1>
+    <p> The content of this list is mostly from
+    <a
+      href="http://discerningspecialist.com/articles/bdsm-101/34-bdsm-checklist" >
+      DiscerningSpecialist
+    </a>.
+    Thanks to them for allowing me to use it and giving me feedback for the page.
+    </p>
+  </section>
 </template>
 
 <script>
