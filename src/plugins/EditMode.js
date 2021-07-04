be5f66dfeeb09fbdba1ded9c69e7cfb54e70f58f
Kleine Schlampe <github.com/SchlampeKleine>
2021-07-04 18:41:09 +0200

    make editmode persistent

--- src/plugins/EditMode.js
+++ src/plugins/EditMode.js
@@ -5,14 +5,17 @@
 
 import { reactive, computed } from 'vue';
 
+const EDITMODEKEY = 'editMode';
+
 const state = reactive({
-  editMode: false,
+  editMode: localStorage.getItem(EDITMODEKEY) || false,
 });
 
 const getEditMode = computed(() => state.editMode);
 
 const toggleEditMode = () => {
   state.editMode = !state.editMode;
+  localStorage.setItem(EDITMODEKEY, state.editMode);
 };
 
 export default () => ({
