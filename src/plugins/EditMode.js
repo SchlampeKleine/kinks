219cf5b6e39da3f0dec0155a0ba95bc450f4732f
Kleine Schlampe <github.com/SchlampeKleine>
2021-07-05 15:40:09 +0200

    add documentation to EditMode

--- src/plugins/EditMode.js
+++ src/plugins/EditMode.js
@@ -4,15 +4,30 @@
  */
 
 import { reactive, computed } from 'vue';
-
+/**
+ * Key used to store the editMode setting
+ */
 const EDITMODEKEY = 'editMode';
 
+/*
+ * The internal state dictionary
+ */
 const state = reactive({
+  /*
+   * State of the editMode
+   * @values true, false
+   */
   editMode: localStorage.getItem(EDITMODEKEY) || false,
 });
 
+/*
+ * Function to get the editMode
+ */
 const getEditMode = computed(() => state.editMode);
 
+/*
+ * Function to toggle the editMode
+ */
 const toggleEditMode = () => {
   state.editMode = !state.editMode;
   localStorage.setItem(EDITMODEKEY, state.editMode);
