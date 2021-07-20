ca331e5a1a7b75c3f16b8f6e2e30014e34b39a28
Kleine Schlampe <github.com/SchlampeKleine>
2021-07-20 19:36:47 +0200

    add generic not found component

---
+++ src/components/NotFound.vue
@@ -0,0 +1,17 @@
+<template>
+  <p>
+    404 SORRY COULDN'T FIND IT!!!
+  </p>
+</template>
+
+<script>
+
+/**
+* Sources
+* - https://code-maze.com/vuejs-routing-navigation/
+*/
+
+export default {
+  name: 'NotFound',
+};
+</script>
