a699de93251c356999b92fe2a9215d94b586fe06
Kleine Schlampe <github.com/SchlampeKleine>
2021-07-24 14:21:55 +0200

    add getter for list (until variants)

--- src/store/modules/CurKinks/getters.js
+++ src/store/modules/CurKinks/getters.js
@@ -10,9 +10,114 @@ export const getCurKinks = (state) => {
     : state.curKinks;
 };
 
-export const getCurKinksAsYAML = (state, getters) => yaml.dump(
-  { categories: state.curKinks.categories },
+export const getCurKinksAsYAML = (getters) => yaml.dump(
+  { categories: getters.getCurKinks.categories },
 );
 
-export const getCurKinksAsURI = (state, getters) =>
-  encodeURI(getters.getCurKinksAsYAML);
+export const getCurKinksAsURI = (state, getters) => encodeURI(getters.getCurKinksAsYAML);
+
+export const getCurKinksAsList = (state, getters) => {
+  const debug = true;
+  const curKinks = getters.getCurKinks.categories;
+
+  if (debug) {
+    console.log({ getCurKinksAsList: curKinks });
+  }
+
+  const parseVariant = (prefix, o, p) => {
+    const h = {
+      category: p.category,
+      subcategory: p.subcategory
+        ? p.subcategory
+        : '',
+      kink: p.kink,
+      variant: o.name,
+    };
+
+    return h;
+  };
+
+  const parseKink = (prefix, o, p) => {
+    const h = {
+      category: p.category,
+      subcategory: p.subcategory
+        ? p.subcategory
+        : '',
+      kink: o.name,
+    };
+    return o.variants
+      ? o.variants
+        .map(
+          (v) => parseVariant(
+            o.name,
+            v,
+            h,
+          ),
+        ).flat()
+      : [h];
+  };
+
+  const parseSubCategory = (prefix, o, p) => {
+    const h = {
+      category: p.category,
+      subcategory: o.name,
+    };
+    return o.kinds
+      .map(
+        (k) => parseKink(
+          o.name,
+          k,
+          h,
+        ),
+      ).flat();
+  };
+
+  const parseCategory = (o) => {
+    const h = {
+      category: o.name,
+    };
+
+    return (
+      o.subcategories
+        ? o
+          .subcategories
+          .map(
+            (s) => {
+              const tmp = parseSubCategory(o.name, s, h);
+              if (debug) {
+                console.log(tmp);
+              }
+              return tmp;
+            },
+          )
+          .flat()
+        : []
+    ).concat(
+      o.kinds
+        ? o.kinds
+          .map(
+            (k) => parseKink(
+              o.name,
+              k,
+              h,
+            ),
+          ).flat()
+        : [],
+    );
+  };
+
+  const tmp = curKinks
+    .map(
+      (o) => {
+        const tmpCategory = parseCategory(o);
+        if (debug) {
+          console.log(tmpCategory);
+        }
+        return tmpCategory;
+      },
+    )
+    .flat();
+
+  console.log({ 'getCurKinksAsList tmp': tmp });
+  return tmp;
+};
