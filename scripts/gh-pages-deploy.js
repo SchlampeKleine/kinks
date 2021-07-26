00612f35a5d9c5fb66605cb35ae8971196789a00
Kleine Schlampe <github.com/SchlampeKleine>
2021-07-26 12:07:55 +0200

    try deploying from worktree

--- scripts/gh-pages-deploy.js
+++ scripts/gh-pages-deploy.js
@@ -5,18 +5,19 @@ const fs = require('fs');
 (async () => {
   try {
     await execa('git', ['stash', 'push']);
-    await execa('git', ['checkout', '--orphan', 'gh-pages']);
+    const folderName = 'autoDeploy';
+    await execa('git', ['worktree', 'add', folderName, '--no-checkout', 'gh-pages']);
     // eslint-disable-next-line no-console
     console.log('Building started...');
     await execa('npm', ['run', 'build', '--production']);
     // Understand if it's dist or build folder
-    const folderName = fs.existsSync('dist') ? 'dist' : 'build';
     await execa('git', ['--work-tree', folderName, 'add', '--all']);
     await execa('git', ['--work-tree', folderName, 'commit', '-m', 'gh-pages']);
     console.log('Pushing to gh-pages...');
     await execa('git', ['push', 'origin', 'HEAD:gh-pages', '--force']);
     await execa('rm', ['-r', folderName]);
-    await execa('git', ['checkout', '-f', 'main']);
+    await execa('git', ['worktree', 'prune']);
+    // await execa('git', ['checkout', '-f', 'main']);
     await execa('git', ['stash', 'pop']);
     await execa('git', ['branch', '-D', 'gh-pages']);
     console.log('Successfully deployed, check your settings');
